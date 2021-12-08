ae106f8c3977df83ea7ad5d62c9eddddc33847f4
Tatiana <ganzhinovat@yandex.ru>
2021-12-08 12:14:44 +0300

    add burger menu

---
+++ scripts/menu.js
@@ -0,0 +1,17 @@
+const openMenu = (nav, classActive) => nav.classList.add(classActive);
+const closeMenu = (nav, classActive) => nav.classList.remove(classActive);
+
+const slideMenu = ({ openBtn, menu, classActiveMenu, closeTrigger }) => {
+    const burgerBtn = document.querySelector(openBtn);
+    const navigation = document.querySelector(menu);
+    const navigationClose = document.querySelectorAll(closeTrigger);
+
+    burgerBtn.addEventListener('click', () => openMenu(navigation, classActiveMenu));
+
+    navigationClose.forEach(item => {
+        item.addEventListener('click', () => closeMenu(navigation, classActiveMenu));
+    });
+};
+
+export default slideMenu;
+
