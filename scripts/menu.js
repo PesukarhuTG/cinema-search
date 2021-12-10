dc5f18ec59c5d27f9ae66a3a009ba8643de344db
Tatiana <ganzhinovat@yandex.ru>
2021-12-10 20:27:01 +0300

    Close a menu with Escape button

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,10 @@
-const openMenu = (nav, classActive) => nav.classList.add(classActive);
-const closeMenu = (nav, classActive) => nav.classList.remove(classActive);
+const openMenu = (nav, classActive) => {
+    nav.classList.add(classActive);
+};
+
+const closeMenu = (nav, classActive) => {
+    nav.classList.remove(classActive);
+};
 
 const slideMenu = ({ openBtn, menu, classActiveMenu, closeTrigger }) => {
     const burgerBtn = document.querySelector(openBtn);
@@ -11,6 +16,14 @@ const slideMenu = ({ openBtn, menu, classActiveMenu, closeTrigger }) => {
     navigationClose.forEach(item => {
         item.addEventListener('click', () => closeMenu(navigation, classActiveMenu));
     });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape') {
+            closeMenu(navigation, classActiveMenu);
+        } else {
+            return;
+        }
+    })
 };
 
 export default slideMenu;
