7d812fbc75851002bae89bed4e7cd9d0eea979e4
Tatiana <ganzhinovat@yandex.ru>
2021-12-12 17:10:27 +0300

    Fix a bug with a no-poster.jpg

--- scripts/renderCards.js
+++ scripts/renderCards.js
@@ -23,7 +23,7 @@ const renderCards = (data, type) => {
         const img = document.createElement('img');
         img.className = 'other-films__img';
         img.alt = `постер ${item.title || item.name}`;
-        img.src = item.poster_path ? `https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${item.poster_path}` : '../img/no-poster.jpg';
+        img.src = item.poster_path ? `https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${item.poster_path}` : './img/no-poster.jpg';
 
         link.append(img);
         card.append(link);
